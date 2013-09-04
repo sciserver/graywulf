4a55c7033672cd2ffe5385d916290fc033d5c43f
Laszlo Dobos <dobos@complex.elte.hu>
2013-09-04 17:50:20 -0400

    Command-line mode workflow host implemented

---
+++ dll/Jhu.Graywulf.Activities/Activities/Enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jhu.Graywulf.Activities
+{
+    /// <summary>
+    /// Possible workflow events
+    /// </summary>
+    public enum WorkflowEventType
+    {
+        Completed,
+        Cancelled,
+        Failed,
+    }
+}
