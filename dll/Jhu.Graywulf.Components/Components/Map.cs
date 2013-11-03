a627133b10fc6cc0f68be09dec03158f41ec7ea2
Laszlo Dobos <dobos@complex.elte.hu>
2013-11-03 16:50:04 +0100

    Added ContainsKey function to Map.

--- dll/Jhu.Graywulf.Components/Components/Map.cs
+++ dll/Jhu.Graywulf.Components/Components/Map.cs
@@ -5,10 +5,15 @@ using System.Text;
 
 namespace Jhu.Graywulf.Components
 {
+    /// <summary>
+    /// Implements a one two one mapping between two sets of objects.
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
     public class Map<T1, T2> : IEnumerable<KeyValuePair<T1, T2>>
     {
-        private Dictionary<T1, T2> dict1 = new Dictionary<T1, T2>();
-        private Dictionary<T2, T1> dict2 = new Dictionary<T2, T1>();
+        private Dictionary<T1, T2> dict1;
+        private Dictionary<T2, T1> dict2;
 
         public T2 this[T1 index]
         {
@@ -22,11 +27,13 @@ namespace Jhu.Graywulf.Components
 
         public Map()
         {
+            this.dict1 = new Dictionary<T1, T2>();
+            this.dict2 = new Dictionary<T2, T1>();
         }
 
-        private void InitializeMembers()
+        public Map(IEqualityComparer<T1> comparer)
         {
-            this.dict1 = new Dictionary<T1, T2>();
+            this.dict1 = new Dictionary<T1, T2>(comparer);
             this.dict2 = new Dictionary<T2, T1>();
         }
 
@@ -34,6 +41,16 @@ namespace Jhu.Graywulf.Components
         {
             dict1.Add(v1, v2);
             dict2.Add(v2, v1);
+        }
+
+        public bool ContainsKey(T1 key)
+        {
+            return dict1.ContainsKey(key);
+        }
+
+        public bool ContainsKey(T2 key)
+        {
+            return dict2.ContainsKey(key);
         }
 
         #region IEnumerable<KeyValuePair<T1,T2>> Members
