2ad41648d94465e901177681a9e2f854b51eebf1
horvatferi <horvatferi@gmail.com>
2013-11-21 19:16:22 +0100

    remove insert, and with clauses from sqlgrammar.cs

--- build/Jhu.Graywulf.SqlParser.Generator/SqlGrammar.cs
+++ build/Jhu.Graywulf.SqlParser.Generator/SqlGrammar.cs
@@ -46,8 +46,6 @@ namespace Jhu.Graywulf.SqlParser.Generator
         public static Expression<Symbol> VectorOpen = () => @"{";
         public static Expression<Symbol> VectorClose = () => @"}";
 
-        public static Expression<Symbol> Apostrophe = () => @"'";
-
         #endregion
         #region Terminals (matched by regular expressions)
 
@@ -300,8 +298,6 @@ namespace Jhu.Graywulf.SqlParser.Generator
             Sequence
             (
                 May(CommentOrWhitespace),   // remove this when wrapped in generic statement grammar
-                May(WithClause),
-                May(InsertClause),
                 QueryExpression,
                 May(CommentOrWhitespace),
                 May(OrderByClause),
@@ -311,7 +307,6 @@ namespace Jhu.Graywulf.SqlParser.Generator
         public static Expression<Rule> QueryExpression = () =>
             Sequence
             (
-                May(WithClause),
                 Must
                 (
                     Sequence(BracketOpen, May(CommentOrWhitespace), QueryExpression, May(CommentOrWhitespace), BracketClose),
@@ -471,7 +466,7 @@ namespace Jhu.Graywulf.SqlParser.Generator
             );
 
         public static Expression<Rule> ColumnAliasList = () =>
-            Sequence(May(CommentOrWhitespace),ColumnAlias, May(Sequence(May(CommentOrWhitespace), Comma, ColumnAliasList)));
+            Sequence(ColumnAlias, May(Sequence(May(CommentOrWhitespace), Comma, ColumnAliasList)));
 
         public static Expression<Rule> TableSampleClause = () =>
             Sequence
@@ -726,71 +721,7 @@ namespace Jhu.Graywulf.SqlParser.Generator
             );
 
         #endregion
-        #region With clause
 
-        public static Expression<Rule> WithClause = () =>
-            Sequence
-            (
-                Keyword("WITH"),
-                CommentOrWhitespace,
-                WithList,
-                May(CommentOrWhitespace)
-            );
-
-        public static Expression<Rule> WithList = () =>
-            Sequence
-            (
-                Must(WithItem),
-                May(Sequence(May(CommentOrWhitespace),Comma,May(CommentOrWhitespace), WithItem))
-                
-            );
-         public static Expression<Rule> WithItem = () =>
-            Sequence
-            (
-                TableAlias,
-                CommentOrWhitespace,
-                Keyword("AS"),
-                May(CommentOrWhitespace),
-                BracketOpen,
-                May(CommentOrWhitespace),
-                QuerySpecification,
-                May(CommentOrWhitespace),
-                BracketClose
-
-            );
-        #endregion
-        #region Insert clause
-
-         public static Expression<Rule> InsertClause = () =>
-             Sequence
-             (
-                 Keyword("INSERT"),
-                 CommentOrWhitespace,
-                 Keyword("INTO"),
-                 CommentOrWhitespace,
-                 TableAlias,
-                 May(CommentOrWhitespace),
-                 BracketOpen,
-                 May(CommentOrWhitespace),
-                 ColumnAliasList,
-                 May(CommentOrWhitespace),
-                 BracketClose,
-                 May(CommentOrWhitespace)
-
-                 /*
-                 Keyword("VALUES"),
-                 May(CommentOrWhitespace),
-                 BracketOpen,
-                 ColumnValueList,
-                 BracketClose*/
-             );
-        public static Expression<Rule> ColumnValueList = () =>
-            Sequence(
-                May(CommentOrWhitespace), 
-                StringConstant, 
-                May(Sequence(May(CommentOrWhitespace), Comma, ColumnValueList))
-            );
-        #endregion
 #if false
 
 
