2521141756e0c8069524bc9f0616b457ac5faa04
Laszlo Dobos <dobos@complex.elte.hu>
2013-12-30 23:29:34 +0100

    New IO task tests added

---
+++ test/Jhu.Graywulf.IO.Test/IO/Tasks/TableCopyTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jhu.Graywulf.Test;
+using Jhu.Graywulf.Schema;
+using Jhu.Graywulf.IO;
+using Jhu.Graywulf.RemoteService;
+
+namespace Jhu.Graywulf.IO.Tasks
+{
+    [TestClass]
+    public class TableCopyTest : TestClassBase
+    {
+        private ITableCopy GetTableCopy(string tableName, bool remote)
+        {
+            ITableCopy q = null;
+            if (remote)
+            {
+                q = RemoteServiceHelper.CreateObject<ITableCopy>(Test.Constants.Localhost);
+            }
+            else
+            {
+                q = new TableCopy();
+            }
+
+            var ds = new Jhu.Graywulf.Schema.SqlServer.SqlServerDataset(Jhu.Graywulf.Test.Constants.TestDatasetName, Jhu.Graywulf.Test.AppSettings.IOTestConnectionString)
+            {
+                IsMutable = true
+            };
+
+            var source = new TableSourceQuery()
+            {
+                Dataset = ds,
+                Query = "SELECT 1 AS one, 2 AS two, 3 AS three"
+            };
+
+            q.Sources = new TableSourceQuery[] { source };
+
+          
+            var destination = new Jhu.Graywulf.Schema.Table()
+            {
+                Dataset = ds,
+                SchemaName = "dbo",
+                TableName = tableName
+            };
+
+            q.Destinations = new Schema.Table[] { destination };
+
+            q.Options = TableInitializationOptions.Create;
+
+            return q;
+        }
+
+        [TestMethod]
+        public void ImportTableTest()
+        {
+            var q = GetTableCopy("TableCopyTest_ImportTableTest", false);
+
+            q.Execute();
+
+            DropTable(q.Destinations[0]);
+        }
+
+        [TestMethod]
+        public void RemoteTableTest()
+        {
+            using (RemoteServiceTester.Instance.GetToken())
+            {
+                RemoteServiceTester.Instance.EnsureRunning();
+                var q = GetTableCopy("TableCopyTest_RemoteTableTest", true);
+
+                q.Execute();
+
+                DropTable(q.Destinations[0]);
+            }
+        }
+    }
+}
