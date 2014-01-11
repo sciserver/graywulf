c53fafd4c4790da6f48a080e34fc6cc61cc52cd9
Laszlo Dobos <dobos@complex.elte.hu>
2014-01-11 16:09:42 -0500

    Attribute to set default date time value added.

---
+++ dll/Jhu.Graywulf.Components/Components/DefaultDateTimeAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Jhu.Graywulf.Components
+{
+    public class DefaultDateTimeAttribute : DefaultValueAttribute
+    {
+        public DefaultDateTimeAttribute()
+            : base(default(DateTime)) { }
+
+        public DefaultDateTimeAttribute(string dateTime)
+            : base(DateTime.Parse(dateTime, System.Globalization.CultureInfo.InvariantCulture)) { }
+    }
+}
