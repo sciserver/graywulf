9acd3adfd776ae8e3e973563a47767d03e7e4933
Laszlo Dobos <dobos@complex.elte.hu>
2014-03-06 18:57:07 +0100

    Missing file added.

---
+++ test/Jhu.Graywulf.Web.Test/Security/OpenIDSettingsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jhu.Graywulf.Registry;
+using Jhu.Graywulf.Web.Security;
+
+namespace Jhu.Graywulf.Web.Test.Security
+{
+    [TestClass]
+    public class OpenIDSettingsTest
+    {
+        [TestMethod]
+        public void SerializeSettingsTest()
+        {
+            var oid = new OpenIDAuthenticator()
+                {
+                    AuthorityName = "Google",
+                    AuthorityUri = "https://www.google.com/accounts/o8/ud",
+                    DisplayName = "GoogleID",
+                    DiscoveryUrl = "https://www.google.com/accounts/o8/id"
+                };
+
+            var p = new Parameter();
+
+            p.Value = new OpenIDAuthenticator[] { oid };
+        }
+    }
+}
