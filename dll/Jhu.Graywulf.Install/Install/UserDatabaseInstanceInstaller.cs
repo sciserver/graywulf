b71ffbefe465963f4092176a77b53947df8ea9ad
Laszlo Dobos <dobos@complex.elte.hu>
2014-06-12 06:15:07 -0400

    Issue when trying to create MyDB on a dead server is fixed.

--- dll/Jhu.Graywulf.Install/Install/UserDatabaseInstanceInstaller.cs
+++ dll/Jhu.Graywulf.Install/Install/UserDatabaseInstanceInstaller.cs
@@ -18,16 +18,14 @@ namespace Jhu.Graywulf.Install
 
         public UserDatabaseInstance CreateUserDatabaseInstance(DatabaseVersion databaseVersion)
         {
-            // TODO: this part probably needs some optimization
-
             // Load server instances that can store user databases
             var ef = new EntityFactory(Context);
-            var sis = ef.FindAll<ServerInstance>()
-                .Where(i => i.ServerVersionReference.Guid == databaseVersion.ServerVersionReference.Guid)
+            var serverInstances = ef.FindAll<ServerInstance>()
+                .Where(i => i.ServerVersionReference.Guid == databaseVersion.ServerVersionReference.Guid && i.Machine.RunningState == RunningState.Running)
                 .OrderBy(i => i.Machine.Number)
                 .ToArray();
 
-            if (sis.Length == 0)
+            if (serverInstances.Length == 0)
             {
                 throw new InvalidOperationException("At least one server instance has to configured for the database version.");    // TODO
             }
@@ -37,33 +35,8 @@ namespace Jhu.Graywulf.Install
             dd.LoadDatabaseInstances(false);
             var dbnum = dd.DatabaseInstances.Count;
 
-            // Find a server that is available
-            int off = 0;
-            int sin = 0;
-            while (off < sis.Length)
-            {
-                sin = (dbnum + 1 + off) % sis.Length;
-                bool ok = true;
-
-                var diag = sis[sin].RunDiagnostics();
-
-                foreach (var d in diag)
-                {
-                    if (d.Status != DiagnosticMessageStatus.OK)
-                    {
-                        ok = false;
-                        break;
-                    }
-                }
-
-
-                if (ok)
-                {
-                    break;
-                }
-
-                off++;
-            }
+            // Pick the nect server that will store the user's myDB
+            var sin = (dbnum + 1) % serverInstances.Length;
 
             // Default slice (FULL)
             dd.LoadSlices(false);
@@ -72,7 +45,7 @@ namespace Jhu.Graywulf.Install
             // Create physical database instance
             var ddi = new DatabaseInstanceInstaller(databaseVersion.DatabaseDefinition);
             var di = ddi.GenerateDatabaseInstance(
-                sis[sin],
+                serverInstances[sin],
                 sl,
                 databaseVersion);
 
