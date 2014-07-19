bf4ac107dcf171c2b820fad6fa14601c5ecbe431
unknown <dobos@GWEN1.sdss.pha.jhu.edu>
2014-07-19 16:12:03 -0400

    ConfigurationReader.cs added.

---
+++ dll/Jhu.Graywulf.Components/Util/ConfigurationReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Jhu.Graywulf.Util
+{
+    public static class ConfigurationReader
+    {
+        public static string GetValue(string group, string key)
+        {
+            var section = (NameValueCollection)ConfigurationManager.GetSection(group);
+            if (section != null)
+            {
+                return (string)section[key];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
