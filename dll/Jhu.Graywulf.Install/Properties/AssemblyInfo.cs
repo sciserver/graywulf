eb079dc487693cd3dc7475fc71b5a7e73f7545ea
Laszlo Dobos <dobos@complex.elte.hu>
2014-07-20 21:36:58 -0400

    Fixed build issues.

--- dll/Jhu.Graywulf.Install/Properties/AssemblyInfo.cs
+++ dll/Jhu.Graywulf.Install/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jhu.Graywulf.Registry.Configuration")]
+[assembly: AssemblyTitle("Jhu.Graywulf.Install")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration(Jhu.Graywulf.Copyright.InfoConfiguration)]
 [assembly: AssemblyCompany(Jhu.Graywulf.Copyright.InfoCompany)]
