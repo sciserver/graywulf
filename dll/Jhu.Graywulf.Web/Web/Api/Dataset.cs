f716ed03ec7887310479a6a05d04917f7ddb6376
Laszlo Dobos <dobos@complex.elte.hu>
2014-07-29 14:38:42 -0400

    DataService can now return datasets.

---
+++ dll/Jhu.Graywulf.Web/Web/Api/Dataset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jhu.Graywulf.Web.Api
+{
+    public class Dataset
+    {
+        public string Name { get; set; }
+    }
+}
