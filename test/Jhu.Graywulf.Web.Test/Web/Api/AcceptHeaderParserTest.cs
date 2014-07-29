532b55b226d9089e85417191b7b9124b9b586ac3
Laszlo Dobos <dobos@complex.elte.hu>
2014-07-29 10:42:18 -0400

    Accept header parser test added.

---
+++ test/Jhu.Graywulf.Web.Test/Web/Api/AcceptHeaderParserTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jhu.Graywulf.Web.Api
+{
+    [TestClass]
+    public class AcceptHeaderParserTest
+    {
+        [TestMethod]
+        public void ParseTest()
+        {
+            var str = "application/vnd.wap.wmlscriptc, text/vnd.wap.wml, application/vnd.wap.xhtml+xml, application/xhtml+xml, text/html, multipart/mixed, */*";
+
+            var mimes = AcceptHeaderParser.Parse(str);
+
+            Assert.AreEqual(7, mimes.Length);
+
+            Assert.AreEqual("application/vnd.wap.wmlscriptc", mimes[0].MimeType);
+            Assert.AreEqual("application/vnd.wap.xhtml+xml", mimes[2].MimeType);
+
+            str = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8,application/json";
+
+            mimes = AcceptHeaderParser.Parse(str);
+
+            Assert.AreEqual(5, mimes.Length);
+
+            Assert.AreEqual("application/xml", mimes[3].MimeType);
+            Assert.AreEqual(0.9, mimes[3].Quality);
+        }
+    }
+}
