cafaa86017ae7f2fcde770dd3a5e4654884a8f94
Laszlo Dobos <dobos@complex.elte.hu>
2014-07-31 17:18:35 -0400

    Missing file added.

---
+++ dll/Jhu.Graywulf.Web/Web/Api/ResourceNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jhu.Graywulf.Web.Api
+{
+    [Serializable]
+    public class ResourceNotFoundException : Exception
+    {
+        public ResourceNotFoundException()
+        {
+        }
+
+        public ResourceNotFoundException(string message)
+            : base(message)
+        {
+        }
+    }
+}
