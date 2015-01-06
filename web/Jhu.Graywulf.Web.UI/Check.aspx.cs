b8433824fe11ec82c47048aaa346eb288fdfa7b9
Laszlo Dobos <dobos@complex.elte.hu>
2015-01-06 12:37:55 -0500

    Added customized e-mail test.

--- web/Jhu.Graywulf.Web.UI/Check.aspx.cs
+++ web/Jhu.Graywulf.Web.UI/Check.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Security;
 using System.IO;
+using Jhu.Graywulf.Web.Security;
 using Jhu.Graywulf.Web.Check;
 
 namespace Jhu.Graywulf.Web.UI
@@ -16,31 +17,44 @@ namespace Jhu.Graywulf.Web.UI
         {
             Checks.Routines.Add(new IdentityCheck());
 
+            // Test registry and log databases
+
             Checks.Routines.Add(new DatabaseCheck(Jhu.Graywulf.Registry.AppSettings.ConnectionString));
             Checks.Routines.Add(new DatabaseCheck(Jhu.Graywulf.Logging.AppSettings.ConnectionString));
+
+            // Test SMTP and target email addresses
 
             Checks.Routines.Add(new EmailCheck(RegistryContext.Domain.Email));
             Checks.Routines.Add(new EmailCheck(RegistryContext.Federation.Email));
 
-            Checks.Routines.Add(new UrlCheck(FormsAuthentication.LoginUrl));
+            if (Request.QueryString["email"] != null)
+            {
+                Checks.Routines.Add(new EmailCheck((string)Request.QueryString["email"]));
+            }
+            
+            // Test sign-in URL
+            var wam = (WebAuthenticationModule)HttpContext.Current.ApplicationInstance.Modules["WebAuthenticationModule"];
+            Checks.Routines.Add(new UrlCheck(wam.GetSignInUrl()));
+            
+            
             Checks.Routines.Add(new UrlCheck("Download", System.Net.HttpStatusCode.Forbidden)); // No directory browsing allowed
 
             Checks.Routines.Add(new EntityCheck(Jhu.Graywulf.Registry.AppSettings.ClusterName));
             Checks.Routines.Add(new EntityCheck(Jhu.Graywulf.Registry.AppSettings.DomainName));
             Checks.Routines.Add(new EntityCheck(Jhu.Graywulf.Registry.AppSettings.FederationName));
 
-            var dir = Jhu.Graywulf.Components.AppSettings.AssemblyPath;
+            var dir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Jhu.Graywulf.Components.AppDomainManager.Configuration.AssemblyPath);
 
             Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Activities.dll")));
             Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Components.dll")));
-            Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Format.dll")));
+            Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Install.dll")));
             Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.IO.dll")));
             Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Jobs.dll")));
             Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Logging.dll")));
-            Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Activities.dll")));
             Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Registry.dll")));
             Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Schema.dll")));
-            Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.SqlParser.dll")));
+            Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Sql.dll")));
+            Checks.Routines.Add(new AssemblyCheck(Path.Combine(dir, "Jhu.Graywulf.Web.dll")));
         }
     }
 }
