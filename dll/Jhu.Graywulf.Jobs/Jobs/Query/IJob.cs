2c4e78a8b18eee6f5fcacab40d88c19af11d6f07
Laszlo Dobos <dobos@complex.elte.hu>
2015-01-19 16:32:49 -0500

    Factored out common job properties into IJob.

---
+++ dll/Jhu.Graywulf.Jobs/Jobs/Query/IJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace Jhu.Graywulf.Jobs.Query
+{
+    public interface IJob
+    {
+        InArgument<Guid> UserGuid { get; set; }
+        InArgument<Guid> JobGuid { get; set; }
+    }
+}
--- dll/Jhu.Graywulf.Jobs/Jobs/Query/IQueryJob.cs
+++ dll/Jhu.Graywulf.Jobs/Jobs/Query/IQueryJob.cs
@@ -13,10 +13,8 @@ namespace Jhu.Graywulf.Jobs.Query
     /// This interface is required by the WebUI which can list all queries in a
     /// unified view
     /// </remarks>
-    public interface IQueryJob
+    public interface IQueryJob : IJob
     {
         InOutArgument<QueryBase> Query { get; set; }
-        InArgument<Guid> UserGuid { get; set; }
-        InArgument<Guid> JobGuid { get; set; }
     }
 }
