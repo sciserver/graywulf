751ff172a2766e1fb629304b2dec760cef810356
Laszlo Dobos <dobos@complex.elte.hu>
2015-03-25 18:43:23 -0400

    User status control does server redirect instead of direct links. Remove cookie before redirect to logout page.

--- dll/Jhu.Graywulf.Web/Web/Controls/UserStatus.cs
+++ dll/Jhu.Graywulf.Web/Web/Controls/UserStatus.cs
@@ -17,10 +17,12 @@ namespace Jhu.Graywulf.Web.Controls
         private Image icon;
         private Label usernameLabel;
         private Label username;
-        private HyperLink account;
-        private HyperLink signOut;
-        private HyperLink signIn;
-        private HyperLink register;
+        private LinkButton account;
+        private LinkButton signOut;
+        private LinkButton signIn;
+        private LinkButton register;
+
+        private WebAuthenticationModule authenticationModule;
 
         [Bindable(false), Themeable(true), UrlProperty]
         public string ImageUrl
@@ -57,52 +59,25 @@ namespace Jhu.Graywulf.Web.Controls
 
             this.username = new Label();
 
-            this.account = new HyperLink();
+            this.account = new LinkButton();
             this.account.Text = Labels.Account;
+            this.account.CausesValidation = false;
+            this.account.Click += Account_Click;
 
-            this.signOut = new HyperLink();
+            this.signOut = new LinkButton();
             this.signOut.Text = Labels.SignOut;
+            this.signOut.CausesValidation = false;
+            this.signOut.Click += SignOut_Click;
 
-            this.signIn = new HyperLink();
+            this.signIn = new LinkButton();
             this.signIn.Text = Labels.SignIn;
+            this.signIn.CausesValidation = false;
+            this.signIn.Click += SignIn_Click;
 
-            this.register = new HyperLink();
+            this.register = new LinkButton();
             this.register.Text = Labels.Register;
-        }
-
-        protected override void OnInit(EventArgs e)
-        {
-            base.OnInit(e);
-
-            Controls.Add(usernameLabel);
-            Controls.Add(username);
-            Controls.Add(account);
-            Controls.Add(signOut);
-            Controls.Add(signIn);
-            Controls.Add(register);
-        }
-
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-
-            username.Text = Page.User.Identity.Name;
-
-            icon.CssClass = CssClass;
-            usernameLabel.CssClass = CssClass;
-            username.CssClass = CssClass;
-            account.CssClass = CssClass;
-            signOut.CssClass = CssClass;
-            signIn.CssClass = CssClass;
-
-            var returl = Server.UrlEncode(Request.RawUrl);
-
-            var wam = (WebAuthenticationModule)HttpContext.Current.ApplicationInstance.Modules["WebAuthenticationModule"];
-
-            account.NavigateUrl = wam.GetUserAccountUrl();
-            signOut.NavigateUrl = wam.GetSignOutUrl();
-            signIn.NavigateUrl = wam.GetSignInUrl();
-            register.NavigateUrl = wam.GetRegisternUrl();
+            this.register.CausesValidation = false;
+            this.register.Click += Register_Click;
         }
 
         protected override void Render(System.Web.UI.HtmlTextWriter writer)
@@ -125,7 +100,7 @@ namespace Jhu.Graywulf.Web.Controls
 
                     username.Text = Page.User.Identity.Name;
                     username.RenderControl(writer);
-                    
+
                     writer.Write(" | ");
                     account.RenderControl(writer);
                     writer.Write(" | ");
@@ -139,5 +114,63 @@ namespace Jhu.Graywulf.Web.Controls
                 }
             }
         }
+
+        #region Event handlers
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Controls.Add(usernameLabel);
+            Controls.Add(username);
+            Controls.Add(account);
+            Controls.Add(signOut);
+            Controls.Add(signIn);
+            Controls.Add(register);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            username.Text = Page.User.Identity.Name;
+
+            icon.CssClass = CssClass;
+            usernameLabel.CssClass = CssClass;
+            username.CssClass = CssClass;
+            account.CssClass = CssClass;
+            signOut.CssClass = CssClass;
+            signIn.CssClass = CssClass;
+
+            authenticationModule = (WebAuthenticationModule)HttpContext.Current.ApplicationInstance.Modules["WebAuthenticationModule"];
+        }
+
+        void Account_Click(object sender, EventArgs e)
+        {
+            var url = authenticationModule.GetUserAccountUrl();
+            Response.Redirect(url);
+        }
+
+        void Register_Click(object sender, EventArgs e)
+        {
+            var url = authenticationModule.GetRegisternUrl();
+            Response.Redirect(url);
+        }
+
+        void SignIn_Click(object sender, EventArgs e)
+        {
+            var url = authenticationModule.GetSignInUrl();
+            Response.Redirect(url);
+        }
+
+        void SignOut_Click(object sender, EventArgs e)
+        {
+            // In case the sing-out page cannot remove cookies
+            // we can do it here
+
+            var url = authenticationModule.GetSignOutUrl();
+
+            authenticationModule.DeleteAuthResponseHeaders();
+            Session.Abandon();
+            Response.Redirect(url);
+        }
+
+        #endregion
     }
 }
