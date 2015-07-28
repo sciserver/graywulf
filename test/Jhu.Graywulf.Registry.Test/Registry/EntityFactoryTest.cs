6a54f40e46c3f8cf106a4bcba8df9e07cc5a970b
Laszlo Dobos <dobos@complex.elte.hu>
2015-07-28 15:45:23 -0400

    Clean up test registry before import.

--- test/Jhu.Graywulf.Registry.Test/Registry/EntityFactoryTest.cs
+++ test/Jhu.Graywulf.Registry.Test/Registry/EntityFactoryTest.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Xml.Serialization;
+using System.Data;
+using System.Data.SqlClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Jhu.Graywulf.Registry
@@ -41,10 +43,23 @@ namespace Jhu.Graywulf.Registry
 
             SaveRegistry(filename);
 
+            // Clean up registry test DB before load
+            using (var cn = new SqlConnection(Jhu.Graywulf.Test.AppSettings.RegistryTestConnectionString))
+            {
+                cn.Open();
+
+                using (var cmd = new SqlCommand("dev.spCleanupEverything", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
             ContextManager.Instance.ConnectionString = Jhu.Graywulf.Test.AppSettings.RegistryTestConnectionString;
 
             using (var context = ContextManager.Instance.CreateContext(ConnectionMode.AutoOpen, TransactionMode.AutoCommit))
             {
+
                 var f = new EntityFactory(context);
 
                 using (var infile = new StreamReader(filename))
