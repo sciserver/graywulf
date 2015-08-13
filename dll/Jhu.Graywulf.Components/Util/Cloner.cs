7c16556821204814c5d572fca0f6c92f848221ee
Laszlo Dobos <dobos@complex.elte.hu>
2015-08-12 21:24:39 -0400

    Cloner util added.

---
+++ dll/Jhu.Graywulf.Components/Util/Cloner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jhu.Graywulf.Util
+{
+    public static class Cloner
+    {
+        public static T Clone<T>(T obj)
+            where T : ICloneable, new()
+        {
+            if (obj == null)
+            {
+                return default(T);
+            }
+            else
+            {
+                return (T)obj.Clone();
+            }
+        }
+    }
+}
