8ea1d17a4f45930b8b8dc2fe81ba4c5481065918
Laszlo Dobos <dobos@complex.elte.hu>
2016-04-16 09:45:23 +0200

    SQL scripts updated.

--- sql/Graywulf.Entities.Sql.Drop.sql
+++ sql/Graywulf.Entities.Sql.Drop.sql
@@ -6,15 +6,17 @@ DROP TYPE [entities].[Identity]
 
 GO
 
-IF (TYPE_ID('[entities].[Acl]') IS NOT NULL)
-DROP TYPE [entities].[Acl]
+IF (TYPE_ID('[entities].[EntityAcl]') IS NOT NULL)
+DROP TYPE [entities].[EntityAcl]
 
 GO
 
+IF EXISTS(SELECT * FROM sys.assemblies WHERE name = 'Jhu.Graywulf.Entities')
 DROP ASSEMBLY [Jhu.Graywulf.Entities]
 
 GO
 
-DROP SCHEMA entities
+IF SCHEMA_ID('entities') IS NOT NULL
+DROP SCHEMA [entities]
 
 GO
