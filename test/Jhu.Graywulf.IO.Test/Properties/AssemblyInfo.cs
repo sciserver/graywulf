5e007e19a7187c92e63fce9ef1892ff89f064177
Laszlo Dobos <dobos@complex.elte.hu>
2016-05-04 11:30:59 +0200

    Copyright and version info fixed.

--- test/Jhu.Graywulf.IO.Test/Properties/AssemblyInfo.cs
+++ test/Jhu.Graywulf.IO.Test/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Jhu.Graywulf.IO.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration(Jhu.Graywulf.Copyright.InfoConfiguration)]
