f18d06aa60dfce2f614ad867ba13f5993d449dc6
Laszlo Dobos <dobos@complex.elte.hu>
2016-05-10 22:18:42 +0200

    Fixed build steps and added inherited class for database key attribute.

---
+++ dll/Jhu.Graywulf.Entities/Entities/Mapping/DbKeyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jhu.Graywulf.Entities.Mapping
+{
+    public class DbKeyAttribute : DbColumnAttribute
+    {
+        public DbKeyAttribute()
+            : base()
+        {
+            this.Binding = DbColumnBinding.Key;
+        }
+
+        public DbKeyAttribute(string name)
+            : base()
+        {
+            this.Binding = DbColumnBinding.Key;
+        }
+    }
+}
