a475a0eba147ab9b712658fbd91834cb022cbd41
Laszlo Dobos <dobos@complex.elte.hu>
2016-05-17 12:53:02 -0400

    Removed comments around web page script includes.

--- web/Jhu.Graywulf.Web.Shared/App_Masters/Basic.master.cs
+++ web/Jhu.Graywulf.Web.Shared/App_Masters/Basic.master.cs
@@ -11,16 +11,10 @@ namespace Jhu.Graywulf.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            this.theScriptManager.Scripts.Add(new ScriptReference(Util.JQuery.JQueryUrl));
+            this.theScriptManager.Scripts.Add(new ScriptReference(Util.Bootstrap.BootstrapUrl));
 
-
-            // temporarily commented out scripts here, no need to build project every time something changes in them
-            // It was also needed to comment out one line in ToolbarButton.cs (Jhu.Graywulf.Web.Controls/Web/Controls)
-
-            //this.theScriptManager.Scripts.Add(new ScriptReference(Util.JQuery.JQueryUrl));
-            //this.theScriptManager.Scripts.Add(new ScriptReference(Util.Bootstrap.BootstrapUrl));
-
-            //this.theScriptManager.Scripts.Add(new ScriptReference("Jhu.Footprint.Web.UI.Scripts.Editor.js", "Jhu.Footprint.Web.UI"));
-            //this.theScriptManager.Scripts.Add(new ScriptReference("Jhu.Graywulf.Web.Controls.DockingPanel.js", "Jhu.Graywulf.Web.Controls"));
+            this.theScriptManager.Scripts.Add(new ScriptReference("Jhu.Graywulf.Web.Controls.DockingPanel.js", "Jhu.Graywulf.Web.Controls"));
 
             this.Page.Title = (string)Page.Application[Constants.ApplicationLongTitle];
             this.Caption.Text = (string)Page.Application[Constants.ApplicationDomainName] + " :: " + (string)Page.Application[Constants.ApplicationShortTitle];
