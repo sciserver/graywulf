aa0a0cbb7e5d1ebc13a6e5975acac6b78ac78063
Evelin Banyai <evelin.banyai@gmail.com>
2016-05-24 15:55:11 +0200

    added PageUtility.cs

---
+++ dll/Jhu.Graywulf.Web/Util/PageUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace Jhu.Graywulf.Util
+{
+    public static class PageUtility
+    {
+        public static Control FindControlRecursive(Control root, string id)
+        {
+            Control f = root.FindControl(id);
+            if (f == null)
+            {
+                foreach (Control c in root.Controls)
+                {
+                    Control x = FindControlRecursive(c, id);
+                    if (x != null) return x;
+                }
+                return null;
+            }
+            else
+            {
+                return f;
+            }
+        }
+    }
+}
