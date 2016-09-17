31d667cbe7e5eb2964ee3633a1a00724717571b3
Laszlo Dobos <dobos@complex.elte.hu>
2016-09-17 18:23:45 -0400

    One step discovery and update function added.

--- dll/Jhu.Graywulf.Registry/Registry/Entity.discover.cs
+++ dll/Jhu.Graywulf.Registry/Registry/Entity.discover.cs
@@ -8,6 +8,17 @@ namespace Jhu.Graywulf.Registry
 {
     public partial class Entity
     {
+        public void Discover()
+        {
+            var update = new List<Entity>();
+            var delete = new List<Entity>();
+            var create = new List<Entity>();
+            Discover(update, delete, create);
+
+            var ef = new EntityFactory(Context);
+            ef.ApplyChanges(update, delete, create);
+        }
+
         /// <summary>
         /// Updates the entity to reflect actual system configuration
         /// </summary>
