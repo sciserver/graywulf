dda4ca69c5b2cb8967798ac0d23e7da2d7add9dd
Laszlo Dobos <dobos@complex.elte.hu>
2016-10-31 12:53:58 -0400

    Updated to new config util.

---
+++ test/Jhu.Graywulf.IO.Test/post-build.ps1
@@ -0,0 +1,11 @@
+﻿param(
+	[string]$SolutionDir,
+	[string]$SolutionName,
+	[string]$ProjectDir,
+	[string]$ProjectName,
+	[string]$OutDir,
+	[string]$TargetName
+)
+
+cp "$ProjectDir..\..\util\eseutil.exe" "$ProjectDir$OutDir"
+cp "$ProjectDir..\..\util\ese.dll" "$ProjectDir$OutDir"
