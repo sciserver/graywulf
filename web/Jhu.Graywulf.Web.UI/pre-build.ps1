3acd298a39a826ad7ad48cb0cab72d38c097414b
Laszlo Dobos <dobos@complex.elte.hu>
2016-10-31 13:36:53 -0400

    Upgraded to new config util.

---
+++ web/Jhu.Graywulf.Web.UI/pre-build.ps1
@@ -0,0 +1,11 @@
+﻿param(
+	[string]$SolutionDir,
+	[string]$SolutionName,
+	[string]$ProjectDir,
+	[string]$ProjectName,
+	[string]$OutDir,
+	[string]$ConfigurationName,
+	[string]$TargetName
+)
+
+& "${SolutionDir}${OutDir}${ConfigurationName}\gwconfig.exe" merge $SolutionDir$SolutionName.sln $ProjectName
