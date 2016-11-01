1affac83fb7cff3c8b87cbbf61b944e74a864213
Laszlo Dobos <dobos@complex.elte.hu>
2016-11-01 19:31:16 -0400

    Missing files added.

---
+++ build/Jhu.Graywulf.Entities.Build/post-build.ps1
@@ -0,0 +1,15 @@
+﻿param(
+	[string]$SolutionDir,
+	[string]$SolutionName,
+	[string]$ProjectDir,
+	[string]$ProjectName,
+	[string]$OutDir,
+	[string]$TargetName
+)
+
+& "${ProjectDir}${OutDir}${TargetName}.exe" "${ProjectDir}..\..\sql\Jhu.Graywulf.Entities\Jhu.Graywulf.Entities.Sql.Create.sql" "${ProjectDir}..\..\dll\Jhu.Graywulf.AccessControl\${OutDir}\Jhu.Graywulf.AccessControl.dll" "${SolutionDir}${OutDir}Jhu.Graywulf.Entities.Sql.Create.sql"
+cp "${ProjectDir}..\..\sql\Jhu.Graywulf.Entities\Jhu.Graywulf.Entities.Sql.Drop.sql" "${SolutionDir}${OutDir}"
+
+
+#$(TargetPath) $(ProjectDir)..\..\sql\Jhu.Graywulf.Entities\Jhu.Graywulf.Entities.Sql.Create.sql $(ProjectDir)..\..\dll\Jhu.Graywulf.AccessControl\$(OutDir)\Jhu.Graywulf.AccessControl.dll $(SolutionDir)$(OutDir)\Jhu.Graywulf.Entities.Sql.Create.sql
+#copy $(ProjectDir)..\..\sql\Jhu.Graywulf.Entities\Jhu.Graywulf.Entities.Sql.Drop.sql $(SolutionDir)$(OutDir)\
