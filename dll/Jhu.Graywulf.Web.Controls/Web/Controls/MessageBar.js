4993480acfdca13583bfdfc1358e556d634fd5ca
Laszlo Dobos <dobos@complex.elte.hu>
2016-12-04 16:05:12 -0500

    Message bar init function fixed.

--- dll/Jhu.Graywulf.Web.Controls/Web/Controls/MessageBar.js
+++ dll/Jhu.Graywulf.Web.Controls/Web/Controls/MessageBar.js
@@ -1,16 +1,20 @@
-﻿function initMessageBar(idx, bar) {
-    var btn = $(bar).find("span.gw-messagebar-button").first();
-    $(btn).on("click", function (event) {
-        var btn = event.target;
-        var bar = $(btn).parent("div").first();
-        $(bar).hide();
-
-        if (dockContents) {
-            $(".dock-container").each(dockContents);
-        }
-    });
-}
-
-function pageLoad() {
+﻿function initMessageBars() {
     $("div.gw-messagebar").each(initMessageBar);
 }
+
+function initMessageBar(idx, bar) {
+    var btn = $(bar).find("span.gw-messagebar-button").first();
+    $(btn).on("click", closeMessageBar);
+}
+
+function closeMessageBar(event) {
+    var btn = event.target;
+    var bar = $(btn).parent("div").first();
+    $(bar).hide();
+
+    if (dockContents) {
+        $(".dock-container").each(dockContents);
+    }
+}
+
+Sys.WebForms.PageRequestManager.getInstance().add_pageLoaded(initMessageBars);
