59ba973258266636a598ab304f92113b9883f356
Laszlo Dobos <dobos@complex.elte.hu>
2017-01-06 10:34:40 -0500

    Assembly info is generated by build utility, so removed from git.

--- web/Jhu.Graywulf.Web.Admin/Properties/AssemblyInfo.cs
+++ web/Jhu.Graywulf.Web.Admin/Properties/AssemblyInfo.cs
@@ -2,19 +2,10 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Jhu.Graywulf.Web.Admin")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration(Jhu.Graywulf.Copyright.InfoConfiguration)]
-[assembly: AssemblyCompany(Jhu.Graywulf.Copyright.InfoCompany)]
-[assembly: AssemblyProduct(Jhu.Graywulf.Copyright.InfoProduct)]
-[assembly: AssemblyCopyright(Jhu.Graywulf.Copyright.InfoCopyright)]
-[assembly: AssemblyTrademark(Jhu.Graywulf.Copyright.InfoTrademark)]
-[assembly: AssemblyCulture(Jhu.Graywulf.Copyright.InfoCulture)]
-
+[assembly: AssemblyDescription("Graywulf Database Federation Toolkit")]
+[assembly: AssemblyCompany("JHU IDIES / ELTE")]
+[assembly: AssemblyProduct("Graywulf")]
+[assembly: AssemblyCopyright("(c) 2008-2017 JHU IDIES / ELTE")]
 [assembly: AssemblyVersion("1.2.6207.1")]
 
-[assembly: ComVisible(false)]
-[assembly: Guid("a9658316-ef70-4fbd-9e10-9b82c5a29c01")]
--- web/Jhu.Graywulf.Web.UI/Properties/AssemblyInfo.cs
+++ web/Jhu.Graywulf.Web.UI/Properties/AssemblyInfo.cs
@@ -2,19 +2,10 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Jhu.Graywulf.Web.UI")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration(Jhu.Graywulf.Copyright.InfoConfiguration)]
-[assembly: AssemblyCompany(Jhu.Graywulf.Copyright.InfoCompany)]
-[assembly: AssemblyProduct(Jhu.Graywulf.Copyright.InfoProduct)]
-[assembly: AssemblyCopyright(Jhu.Graywulf.Copyright.InfoCopyright)]
-[assembly: AssemblyTrademark(Jhu.Graywulf.Copyright.InfoTrademark)]
-[assembly: AssemblyCulture(Jhu.Graywulf.Copyright.InfoCulture)]
-
+[assembly: AssemblyDescription("Graywulf Database Federation Toolkit")]
+[assembly: AssemblyCompany("JHU IDIES / ELTE")]
+[assembly: AssemblyProduct("Graywulf")]
+[assembly: AssemblyCopyright("(c) 2008-2017 JHU IDIES / ELTE")]
 [assembly: AssemblyVersion("1.2.6207.1")]
 
-[assembly: ComVisible(false)]
-[assembly: Guid("ad7dcedf-092d-4741-836f-b9a76ca54738")]
