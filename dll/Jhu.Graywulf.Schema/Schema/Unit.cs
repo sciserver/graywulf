be11f69f8b028e0f950e93644b80017a6508c3a8
ebanyai <evelin.banyai@gmail.com>
2017-01-24 08:52:55 -0500

    Added Unit and UnitPart classes

---
+++ dll/Jhu.Graywulf.Schema/Schema/Unit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Jhu.Graywulf.Schema
+{
+    public class Unit
+    {
+        private double factor;
+        private List<UnitPart> parts;
+
+        public double Factor
+        {
+            get { return factor; }
+            set { factor = value; }
+        }
+        public List<UnitPart> Parts
+        {
+            get { return parts; }
+        }
+
+        public Unit()
+        {
+            initalizeMembers();
+        }
+
+        public Unit(string unitString)
+        {
+            initalizeMembers();
+            parseUnitString(unitString);
+        }
+
+        private void initalizeMembers()
+        {
+            factor = 1.0;
+            parts = new List<UnitPart>();
+        }
+
+        private void parseUnitString(string unitString)
+        {
+            var parts =  unitString.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            var first = 0;
+            if (double.TryParse(parts[0], out factor))
+            {
+                first = 1;
+            }
+
+            for (int i=first; i < parts.Length; i++ )
+            {
+                this.parts.Add(new UnitPart(parts[i]));
+            }
+
+        }
+
+    }
+}
---
+++ dll/Jhu.Graywulf.Schema/Schema/UnitPart.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Jhu.Graywulf.Schema
+{
+    public class UnitPart
+    {
+        private string prefix;
+        private string unitBase;
+        private string exponent;
+        private string function;
+
+        public string Prefix
+        {
+            get { return prefix; }
+            set { prefix = value; }
+        }
+
+        public string Unit
+        {
+            get { return unitBase; }
+            set { unitBase = value; }
+        }
+
+        public string Exponent
+        {
+            get { return exponent; }
+            set { exponent = value; }
+        }
+
+        public string Function
+        {
+            get { return function; }
+            set { function = value; }
+        }
+
+        public UnitPart()
+        {
+            initializeMembers();
+        }
+
+        public UnitPart(string unit)
+        {
+            parseUnitPartString(unit);
+        }
+        private void initializeMembers()
+        {
+            prefix = "";
+            unitBase = "";
+            exponent = "";
+            function = "";
+        }
+
+
+        private void parseUnitPartString(string part)
+        {
+            Regex sPattern = new Regex(@"(?<base>\w+)(?<exp>[\+-]\d*[/]?\d*)?"); //
+
+            Match m = sPattern.Match(part);
+            unitBase = m.Groups["base"].Value;
+            //TODO: prefix detection
+
+            exponent = m.Groups["exp"].Value;
+        }
+    }
+}
