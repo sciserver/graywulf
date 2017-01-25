1a3356b56541126e2486d328b14d9b8e7ac18820
ebanyai <evelin.banyai@gmail.com>
2017-01-25 04:45:12 -0500

    Added UnitTest project + class to test unit parsing

---
+++ test/Jhu.Graywulf.Schema.Test/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Jhu.Graywulf.Schema
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void ParseUnitPartStringTest()
+        {
+            var part = new UnitPart("arcsec");
+
+        }
+
+        [TestMethod]
+        public void ParseUnitStringTest()
+        {
+            var unitString = "1e-27 erg s-1 cm-2 AA-1";
+            var unit = new Unit(unitString);
+
+        }
+    }
+}
