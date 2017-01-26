4cf4901fbaa6a8af9a81902c321b8b18941fe801
ebanyai <evelin.banyai@gmail.com>
2017-01-26 08:32:43 -0500

    added new tests

--- test/Jhu.Graywulf.Schema.Test/UnitTest.cs
+++ test/Jhu.Graywulf.Schema.Test/UnitTest.cs
@@ -9,18 +9,39 @@ namespace Jhu.Graywulf.Schema
     public class UnitTest
     {
         [TestMethod]
-        public void ParseUnitPartStringTest()
+        public void ParseUnitPartTest1()
         {
             var part = new UnitPart("arcsec");
 
+            Assert.AreEqual(part.UnitBase, "arcsec");
         }
 
         [TestMethod]
-        public void ParseUnitStringTest()
+        public void ParseUnitPartTest2()
         {
-            var unitString = "1e-27 erg s-1 cm-2 AA-1";
+            var part = new UnitPart("km-2");
+            var tPart = new UnitPart() { UnitBase = "m", Exponent = "-2", Prefix = "k"};
+
+            Assert.AreEqual(part.ToString(), tPart.ToString());
+        }
+
+
+        [TestMethod]
+        public void ParseUnitPartTest3()
+        {
+            var part = new UnitPart("log(umag)-1");
+            var tPart = new UnitPart() { UnitBase = "mag", Exponent = "-1", Prefix = "u", Function="log" };
+
+            Assert.AreEqual(part.ToString(), tPart.ToString());
+        }
+
+        [TestMethod]
+        public void ParseUnitTest()
+        {
+            var unitString = "1E-27 erg s-1 cm-2 AA-1";
             var unit = new Unit(unitString);
 
+            Assert.AreEqual(unitString, unit.ToString());
         }
     }
 }
