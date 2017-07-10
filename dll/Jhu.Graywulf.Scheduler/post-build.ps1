a9727283201bc51eb252ae6d2d043b8b5366f57a
Laszlo Dobos <dobos@complex.elte.hu>
2017-07-10 00:06:14 -0400

    Fixed build issues

--- dll/Jhu.Graywulf.Scheduler/post-build.ps1
+++ dll/Jhu.Graywulf.Scheduler/post-build.ps1
@@ -1,3 +1,2 @@
-﻿cp $ProjectDir$OutDir$TargetName.exe $SolutionDir$OutDir
+﻿cp $ProjectDir$OutDir$TargetName.dll $SolutionDir$OutDir
 cp $ProjectDir$OutDir$TargetName.pdb $SolutionDir$OutDir
-cp $ProjectDir$OutDir$TargetName.exe.config $SolutionDir$OutDir
