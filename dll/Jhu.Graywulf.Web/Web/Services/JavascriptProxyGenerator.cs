629bd204385143e0ab57494965060dfc6772d6f4
Laszlo Dobos <dobos@complex.elte.hu>
2017-07-29 14:41:47 -0400

    Write constant query parts correctly.

--- dll/Jhu.Graywulf.Web/Web/Services/JavascriptProxyGenerator.cs
+++ dll/Jhu.Graywulf.Web/Web/Services/JavascriptProxyGenerator.cs
@@ -36,13 +36,14 @@ namespace Jhu.Graywulf.Web.Services
 
         protected override void WriteMethod(TextWriter writer, MethodInfo method, ParameterInfo[] parameters, ParameterInfo bodyParameter, string httpMethod, RestUriTemplate uriTemplate)
         {
+            var methodName = GetMethodName(method);
             var script = new StringBuilder(Templates.Javascript.Method);
             var parameterList = GetParameterList(parameters);
             var pathParts = GetPathPartsList(uriTemplate);
             var queryParts = GetQueryPartsList(uriTemplate);
 
             script.Replace("__serviceName__", ServiceName);
-            script.Replace("__methodName__", GetMethodName(method));
+            script.Replace("__methodName__", methodName);
             script.Replace("__parameterList__", parameterList);
             script.Replace("__bodyParameter__", bodyParameter?.Name ?? "null");
             script.Replace("__returnType__", method.ReturnType == typeof(void) ? "\"text\"" : "\"json\"");
@@ -101,15 +102,20 @@ namespace Jhu.Graywulf.Web.Services
 
             for (int i = 0; i < uriTemplate.QueryKeys.Length; i++)
             {
+                if (res != "")
+                {
+                    res += ", ";
+                }
+
                 if (uriTemplate.IsQueryParameter(i))
                 {
-                    if (res != "")
-                    {
-                        res += ", ";
-                    }
-
                     res += "\"" + uriTemplate.QueryKeys[i] + "\"" +
                         ": " + uriTemplate.QueryValues[i].TrimStart('{').TrimEnd('}');
+                }
+                else
+                {
+                    res += "\"" + uriTemplate.QueryKeys[i] + "\"" +
+                        ": \"" + uriTemplate.QueryValues[i] + "\"";
                 }
             }
 
