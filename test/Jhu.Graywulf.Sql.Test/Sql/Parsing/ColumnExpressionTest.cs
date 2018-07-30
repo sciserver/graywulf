cebd5a2ee689fa709f5a8da3e14002793203afe3
Laszlo Dobos <dobos@complex.elte.hu>
2018-07-30 13:25:50 +0200

    Added back missing test.

---
+++ test/Jhu.Graywulf.Sql.Test/Sql/Parsing/ColumnExpressionTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jhu.Graywulf.Sql.Parsing
+{
+    [TestClass]
+    public class ColumnExpressionTest : ParsingTestBase
+    {
+        [TestMethod]
+        public void SetUserVariableTest()
+        {
+            var ce = Parse<ColumnExpression>("@var = 1");
+            Assert.AreEqual("@var", ce.AssignedVariable.Value);
+            Assert.AreEqual("1", ce.Expression.Value);
+        }
+
+        [TestMethod]
+        public void SetColumnAliasTest()
+        {
+            var ce = Parse<ColumnExpression>("alias = tab.col");
+            Assert.AreEqual("alias", ce.ColumnReference.ColumnAlias);
+            Assert.AreEqual("tab.col", ce.Expression.Value);
+        }
+
+        [TestMethod]
+        public void SimpleColumnWithoutAliasTest()
+        {
+            var ce = Parse<ColumnExpression>("tab.col");
+            Assert.AreEqual(null, ce.ColumnReference.ColumnAlias);
+            Assert.AreEqual("tab.col", ce.Expression.Value);
+        }
+
+        [TestMethod]
+        public void SimpleColumnAliasTest()
+        {
+            var ce = Parse<ColumnExpression>("tab.col alias");
+            Assert.AreEqual("alias", ce.ColumnReference.ColumnAlias);
+            Assert.AreEqual("tab.col", ce.Expression.Value);
+
+            ce = Parse<ColumnExpression>("tab.col[alias]");
+            Assert.AreEqual("alias", ce.ColumnReference.ColumnAlias);
+            Assert.AreEqual("tab.col", ce.Expression.Value);
+
+            ce = Parse<ColumnExpression>("tab.col AS alias");
+            Assert.AreEqual("alias", ce.ColumnReference.ColumnAlias);
+            Assert.AreEqual("tab.col", ce.Expression.Value);
+
+            ce = Parse<ColumnExpression>("tab.col AS [alias]");
+            Assert.AreEqual("alias", ce.ColumnReference.ColumnAlias);
+            Assert.AreEqual("tab.col", ce.Expression.Value);
+
+            ce = Parse<ColumnExpression>("tab.[col]AS[alias]");
+            Assert.AreEqual("alias", ce.ColumnReference.ColumnAlias);
+            Assert.AreEqual("tab.[col]", ce.Expression.Value);
+        }
+
+        [TestMethod]
+        public void CreateSingleStarTest()
+        {
+            var ce = ColumnExpression.CreateStar();
+            Assert.AreEqual("*", ce.Value);
+            Assert.IsTrue(ce.ColumnReference.IsStar);
+            Assert.IsTrue(ce.ColumnReference.TableReference.IsUndefined);
+        }
+
+        [TestMethod]
+        public void CreateTableStarTest()
+        {
+            var cg = CreateCodeGenerator();
+            var tr = new NameResolution.TableReference()
+            {
+                TableName = "test"
+            };
+            var ce = ColumnExpression.CreateStar(tr);
+            Assert.AreEqual("[test].*", cg.Execute(ce));
+            Assert.IsTrue(ce.ColumnReference.IsStar);
+            Assert.AreEqual("test", ce.ColumnReference.TableReference.TableName);
+        }
+
+        [TestMethod]
+        public void CreateFromExpressionTest()
+        {
+            var exp = new SqlParser().Execute<Expression>("a + b");
+            var ce = ColumnExpression.Create(exp, "alias");
+            Assert.AreEqual("a + b AS alias", ce.Value);
+        }
+    }
+}
